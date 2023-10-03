3f11b7b9ce48d1437c34d5b4dc62a9477ef01c99
Ons BEN SALAH <ons.bensalah@esprit.tn>
2023-10-03 07:41:04 +0100

    new class User.java by feature branch

---
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -0,0 +1,28 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level=AccessLevel.PRIVATE)
+@Entity
+public class User implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Long numUser;
+	
+}
